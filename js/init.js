bc308e0f4bda474617fccf0ccf5137ca2be8b824
Uniliva <uniliva-ap@hotmail.com>
2018-03-28 15:38:59 -0300

    Correções solicitadas pelo Jhon Lenon e adição do help

--- js/init.js
+++ js/init.js
@@ -9,3 +9,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+var current_hash = location.hash,
+current_hash = current_hash.replace(/#/,''),
+to_url = 'https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s';
+if(current_hash.length>1){
+// we have something after the #. redirect as if the # wasn't there.
+    if(current_hash =="help"){
+    window.location.href=(to_url);
+    console.log(current_hash);
+    }
+}
