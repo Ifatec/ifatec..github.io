6c64d713911d086daeb38964cfff1268e02fae41
Uniliva <uniliva-ap@hotmail.com>
2018-03-28 15:38:59 -0300

    Correções solicitadas pelo Jhon Lenon e adição do help

--- js/init.js
+++ js/init.js
@@ -1,11 +1,19 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
-      
-      $("#menu").on('click', function(event){
-          $("#menu-painel").removeClass("topo-fixo");
-      });
+
+    $("#menu").on('click', function (event) {
+      $("#menu-painel").removeClass("topo-fixo");
+    });
 
   }); // end of document ready
+
+  var current_hash = location.hash.replace(/#/, '');
+  var to_url = 'https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s';
+
+  if (current_hash == "help-me") {
+    window.location.href = (to_url);
+  }
 })(jQuery); // end of jQuery name space
+
