2dcb4f055bf3fe3aa7ac741514063b8f76ed5b87
Uniliva <uniliva-ap@hotmail.com>
2018-03-29 11:37:46 -0300

    Ajustando scroll e menu mobile

--- js/init.js
+++ js/init.js
@@ -4,22 +4,36 @@
     $('.button-collapse').sideNav();
 
     $("#menu").on('click', function (event) {
-      $("#menu-painel").removeClass("topo-fixo");
+      $("#sidenav-overlay").css("position", "unset");
     });
+
+    $("#nav-mobile a").on('click', function (event) {
+      $('.button-collapse').sideNav('hide');
+    });
+
 
   }); // end of document ready
 
 })(jQuery); // end of jQuery name space
 
-function redirecionaUrl() {
-  var current_hash = location.hash.replace(/#/,'');
+$('.menu-teste').click(function () {
+  var id = $(this).attr("value");
+  $('html, body').animate({
+    scrollTop: $(id).offset().top
+  }, 1500);
+});
 
-  if(current_hash.length > 1 && current_hash == 'help-me') {
-      window.location.href = "https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s";
+
+
+function redirecionaUrl() {
+  var current_hash = location.hash.replace(/#/, '');
+
+  if (current_hash.length > 1 && current_hash == 'help-me') {
+    window.location.href = "https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s";
   }
 }
 
-window.onhashchange = function() { 
+window.onhashchange = function () {
   redirecionaUrl();
 };
 
