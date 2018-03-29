cd77e148542b9ee10ae3324ca069cf0306e3ea87
Uniliva <uniliva-ap@hotmail.com>
2018-03-29 08:00:51 -0300

    Finalizando projeto web

--- js/init.js
+++ js/init.js
@@ -9,11 +9,18 @@
 
   }); // end of document ready
 
-  var current_hash = location.hash.replace(/#/, '');
-  var to_url = 'https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s';
-
-  if (current_hash == "help-me") {
-    window.location.href = (to_url);
-  }
 })(jQuery); // end of jQuery name space
 
+function redirecionaUrl() {
+  var current_hash = location.hash.replace(/#/,'');
+
+  if(current_hash.length > 1 && current_hash == 'help-me') {
+      window.location.href = "https://drive.google.com/drive/folders/0B9Lw4TKJ5vl6VzRVWnI2RlFPV2s";
+  }
+}
+
+window.onhashchange = function() { 
+  redirecionaUrl();
+};
+
+redirecionaUrl();
